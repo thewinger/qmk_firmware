438ec7494987ef32bdfc6fc08a30db0ae5ea1f3f
thewinger <winger.87@gmail.com>
2019-03-10 20:54:07 +0100

    added led functionality

--- keyboards/dz60/keymaps/thewinger/config.h
+++ keyboards/dz60/keymaps/thewinger/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Turn oof RGB/Underglow when computer goes to sleep
+// Turn off RGB/Underglow when computer goes to sleep
 #ifdef RGBLIGHT_ENABLE
 #define RGBLIGHT_SLEEP
 #endif // RGBLIGHT_ENABLE
--- keyboards/dz60/keymaps/thewinger/keymap.c
+++ keyboards/dz60/keymaps/thewinger/keymap.c
@@ -13,29 +13,14 @@ enum layers {
   _NAVM,
   _LEDS,
   _CONFIG
-}
+};
 
-/*layout template
- * ,-----------------------------------------------------------------------------------------.
- * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
- * |-----------------------------------------------------------------------------------------+
- * |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
- * |-----------------------------------------------------------------------------------------+
- * |          |     |     |     |     |     |     |     |     |     |     |     |            |
- * |-----------------------------------------------------------------------------------------+
- * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
- * |-----------------------------------------------------------------------------------------+
- * |      |      |      |                                      |       |       |      |      |
- * `-----------------------------------------------------------------------------------------'
-
-
- [LAYOUT] = LAYOUT_all(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______,
-  _______, X,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______,           _______,        _______,           _______,         _______, _______, _______, _______, _______),
-*/
+enum keycodes {
+    WIN = SAFE_RANGE,
+    MAC,
+    NAVM,
+    LEDS,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -58,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,             KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         LT(_NAVM, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
         KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    RSFT_T(KC_UP),
-        KC_LCTL,  KC_LGUI,  KC_LALT,                     KC_SPC,                    _LEDS,     RALT_T(KC_LEFT),    RGUI_T(KC_DOWN),    RCTL_T(KC_RIGHT)),
+        KC_LCTL,  KC_LGUI,  KC_LALT,                     KC_SPC,                    LEDS,     RALT_T(KC_LEFT),    RGUI_T(KC_DOWN),    RCTL_T(KC_RIGHT)),
 
     /* MAC
      * ,-----------------------------------------------------------------------------------------.
@@ -77,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT_all(
         KC_GESC,            KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,             KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
-        LT(_FUNC, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
+        LT(_NAVM, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
         KC_LSFT,            KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    RSFT_T(KC_UP),
-        KC_LCTL,  KC_LALT,  KC_LGUI,                     KC_SPC,                    _FUNC2,     RALT_T(KC_LEFT),    RGUI_T(KC_DOWN),    RCTL_T(KC_RIGHT)),
+        KC_LCTL,  KC_LALT,  KC_LGUI,                     KC_SPC,                    LEDS,     RALT_T(KC_LEFT),    RGUI_T(KC_DOWN),    RCTL_T(KC_RIGHT)),
 
 
      /* NAVMEDIA
@@ -88,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------------------------------------+
      * |        |     |  Up |     |     |     |     |     |     |     |     |  T- |  T+ |  Play  |
      * |-----------------------------------------------------------------------------------------+
-     * |          |  Lf |  Dw |  Rg |     |     |     |     |     |     |  V- | V+  |    M       |
+     * |          |  Lf |  Dw |  Rg |     |     |     |     |     |     |  V- | V+  |    Mute    |
      * |-----------------------------------------------------------------------------------------+
      * |           |     |     |     |     |     |     |     |     |     |     |                 |
      * |-----------------------------------------------------------------------------------------+
@@ -107,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------------.
      * | Wake| F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10  |  F11  | F12  |  Del  |
      * |-----------------------------------------------------------------------------------------+
-     * |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+     * |        |RGBTo|RGBMo|RGBH+|RGBH-|RGBS+|RGBS-|RGBV+|RGBV-|     |     |     |     |        |
      * |-----------------------------------------------------------------------------------------+
-     * |          |     |     |     |     |     |     |     |     |     |     |     |            |
+     * |          | BL- | BTo | BL+ |BLStep|    |     |     |     |     |     |     |            |
      * |-----------------------------------------------------------------------------------------+
      * |           |     |     |     |     |     |     |     |     |     |     |                 |
      * |-----------------------------------------------------------------------------------------+
@@ -121,10 +106,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_WAKE, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     KC_DEL,
         _______, RGB_TOG, RGB_MOD,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,  _______,    _______,    _______,
         _______, BL_DEC,  BL_TOGG,  BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,  _______,    _______,
-        _______, _______, _______,  _______,    _______, _______, _______, _______, _______, _______, _______,  _______,
-        _______, _______, _______,                      _______,                    _______, _______,           _______,             _______),
+        _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______,     _______,
+        _______, _______, _______,                      _______,                    _______,     _______,       _______,             _______),
 
-        /* CONFIG
+        /* CONFIG (NAVMED + LEDS)
      * ,-----------------------------------------------------------------------------------------.
      * |     |     |     |     |     |     |     |     |     |     |      |       |      | Reset |
      * |-----------------------------------------------------------------------------------------+
@@ -138,11 +123,100 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_LEDS] = LAYOUT_all(
+    [_CONFIG] = LAYOUT_all(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
-        _______, _______, _WIN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _MAC, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,    _______,
-        _______, _______, _______,  _______,    _______, _______, _______, _______, _______, _______, _______,  _______,
-        _______, _______, _______,                      _______,                    _______, _______,           _______,             _______)
+        _______, _______, WIN,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, MAC,     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,    _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______,
+        _______, _______, _______,                      _______,                    _______,      _______,     _______,      _______)
 
+};
+
+void keyboard_post_init_rgb(void) {
+    #ifdef RBGLIGHT_ENABLE
+        rgblight_enable_noeeprom();
+        switch (biton32(eeconfig_read_default_layer())) {
+            case _WIN:
+                rgblight_sethsv_noeeprom(180, 255, 255): // sets the color to teal/cyan without saving
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3); // sets mode to Fast breathing without saving
+                break;
+
+            case _MAC:
+                rgblight_sethsv_noeeprom(180, 255, 255): // sets the color to teal/cyan without saving
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT); // sets mode to Fast breathing without saving
+                break;
+
+            default:
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT);
+        }
+}
+
+// switch layers depending on keypressed, with persistent layer in the mix
+bool process_record_keymap(uint16t keycode, keyrecord_t *record) {
+    switch (keycode)
+    {
+        case WIN:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_WIN);
+            }
+            return false;
+            break;
+
+        case MAC:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_MAC);
+            }
+            return false;
+            break;
+
+        case NAVM:
+            if (record->event.pressed) {
+                layer_on(_NAVM);
+                update_tri_layer(_NAVM, _LEDS, _CONFIG);
+            } else  {
+                layer_off(_NAVM);
+                update_tri_layer(_NAVM, _LEDS, _CONFIG);
+            }
+            return false;
+            break;
+
+         case LEDS:
+            if (record->event.pressed) {
+                layer_on(_LEDS);
+                update_tri_layer(_NAVM, _LEDS, _CONFIG);
+            } else  {
+                layer_off(_LEDS);
+                update_tri_layer(_NAVM, _LEDS, _CONFIG);
+            }
+            return false;
+            break;
+
+       default:
+            break;
+    }
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+    #ifdef RGBLIGHT_ENABLE
+        switch (biton32(state)) {
+            case _WIN:
+                rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+                rgblight_sethsv_noeeprom(180, 255, 255): // sets the color to teal/cyan without saving
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3); // sets mode to Fast breathing without saving
+                break;
+
+            case _MAC:
+                rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+                rgblight_sethsv_noeeprom(180, 255, 255): // sets the color to teal/cyan without saving
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT); // sets mode to Fast breathing without saving
+                break;
+
+            default:
+                rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+                rgblight_sethsv_noeeprom(180, 255, 255): // sets the color to teal/cyan without saving
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT); // sets mode to Fast breathing without saving
+                break;
+        }
+    #endif // RGBLIGHT_ENABLE
+    return state
 }
