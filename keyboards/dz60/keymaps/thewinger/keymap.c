0514e218af9a426a41adb59f2027be12334a4e6e
thewinger <winger.87@gmail.com>
2019-03-11 08:19:27 +0100

    change layout to 60 ansi

--- keyboards/dz60/keymaps/thewinger/keymap.c
+++ keyboards/dz60/keymaps/thewinger/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_WIN] = LAYOUT_all(
+    [_WIN] = LAYOUT_60_ansi(
         KC_GESC,            KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,             KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         LT(_NAVM, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_MAC] = LAYOUT_all(
+    [_MAC] = LAYOUT_60_ansi(
         KC_GESC,            KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,             KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         LT(_NAVM, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_NAVM] = LAYOUT_all(
+    [_NAVM] = LAYOUT_60_ansi(
         KC_WAKE, KC_F1,   KC_F2,    KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     KC_DEL,
         _______, _______, KC_UP,    _______,    _______, _______, _______, _______, _______, _______, _______,  KC_MRWD,    KC_MFFD,    KC_MPLY,
         _______, KC_LEFT, KC_DOWN,  KC_RIGHT,   _______, _______, _______, _______, _______, _______, KC_VOLD,  KC_VOLU,    KC_MUTE,
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_LEDS] = LAYOUT_all(
+    [_LEDS] = LAYOUT_60_ansi(
         KC_WAKE, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     KC_DEL,
         _______, RGB_TOG, RGB_MOD,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,  _______,    _______,    _______,
         _______, BL_DEC,  BL_TOGG,  BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,  _______,    _______,
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------------'
      */
 
-    [_CONFIG] = LAYOUT_all(
+    [_CONFIG] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
         _______, _______, WIN,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, MAC,     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,    _______,
