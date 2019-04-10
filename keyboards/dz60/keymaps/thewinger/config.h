8371148ca519a783c86ae0509d8d3fe5c6c8e228
thewinger <winger.87@gmail.com>
2019-04-10 12:34:37 +0200

    removes combo code

--- keyboards/dz60/keymaps/thewinger/config.h
+++ keyboards/dz60/keymaps/thewinger/config.h
@@ -4,6 +4,3 @@
 #ifdef RGBLIGHT_ENABLE
 #define RGBLIGHT_SLEEP
 #endif // RGBLIGHT_ENABLE
-
-#define COMBO_COUNT 1
-#define COMBO_TERM 200
--- keyboards/dz60/keymaps/thewinger/rules.mk
+++ keyboards/dz60/keymaps/thewinger/rules.mk
@@ -2,4 +2,3 @@ BOOTMAGIC_ENABLE = no # Virtual DIP switch configuration (+1000)
 MOUSEKEY_ENABLE = no # Mouse Keys (+4700)
 RGBLIGHT_ENABLE = yes
 CONSOLE_ENABLE = yes
-COMBO_ENABLE = yes
